fb31ce41a5218248a19e7dfe0c8d44c491c42129
RomanBuletsa <r.buletsa@gmail.com>
2017-10-21 17:48:00 +0300

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin@test.com', password: '88888888', password_confirmation: '88888888') if Rails.env.development?
+AdminUser.create!(email: 'admin@test1.com', password: '99999999', password_confirmation: '99999999') if Rails.env.production?
